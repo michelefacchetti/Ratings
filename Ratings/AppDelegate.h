0edf6f6691b949d0e4ef42616a9f0d7610dfbd2e
developer <developer@MacBook-Pro-di-Michele.local>
2014-07-09 16:48:09 +0200

    Initial Commit

---
+++ Ratings/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ratings
+//
+//  Created by developer on 09/07/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ratings/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ratings
+//
+//  Created by developer on 09/07/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
